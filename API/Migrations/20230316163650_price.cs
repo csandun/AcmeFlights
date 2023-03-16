3c6e46d28aa723165bd09634bb567673f2d7df35
csandun <csandun.personal@gmail.com>
2023-03-17 00:16:22 +0530

    🚩 add missing files

---
+++ API/Migrations/20230316163650_price.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class price : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Price_Currency",
+                table: "FlightRates",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price_Value",
+                table: "FlightRates",
+                type: "numeric",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price_Currency",
+                table: "FlightRates");
+
+            migrationBuilder.DropColumn(
+                name: "Price_Value",
+                table: "FlightRates");
+        }
+    }
+}
